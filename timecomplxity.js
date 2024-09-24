fe7ccea4bce74df0c0f4354f8fbb608bc2c781a3
Vedant®™ <109432118+VenomMonstee@users.noreply.github.com>
2024-09-24 21:58:59 +0530

    Add files via upload
    All  about Time complexity

---
+++ timecomplxity.js
@@ -0,0 +1,13 @@
+/*
+1. Constant Time O(1) :
+      Here the work is done in fixed time, no matter what the input is.
+      Like opening the fridge and taking out an item, the time is fixed.
+
+      "Explanation:
+Chahe array mein 10 elements ho ya 10,000, tu sirf pehle element ko access kar raha hai, time fixed hai. Yeh hota hai O(1), constant time."
+
+*/
+
+function getFirstElement(arr){
+    return arr[0];
+ }
